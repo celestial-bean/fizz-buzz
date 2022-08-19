46a734927e00a13bac252d603aebc485750a7ec8
darknessMaker <92747680+darknessMaker@users.noreply.github.com>
2022-08-19 16:30:47 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var final=[]
+var limit=prompt("How many digits?")
+function fizzbuzz(i) {
+  if (i % 3 == 0) {
+    if (i % 5 == 0) {
+      return " fizzbuzz";
+    } else {
+      return " fizz ";
+    }
+  }else if (i%5==0){
+  return "buzz "
+  }else{
+  return ` ${i}`;
+  }
+}
+
+for (var i = 1; i <= limit; i++) {
+  final.push(fizzbuzz(i))
+}
+document.querySelector("a").textContent = final
