539534bbbe82be1c5ca01cf752c681081195a2bd
Logan <92747680+celestial-bean@users.noreply.github.com>
2023-08-05 14:25:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var final=[]
-var limit=prompt("How many digits?")
+var final=[];
+var limit=prompt("How many digits?");
 function fizzbuzz(i) {
   if (i % 3 == 0) {
     if (i % 5 == 0) {
-      return " fizzbuzz";
+      return "fizzbuzz";
     } else {
-      return " fizz ";
+      return "fizz";
     }
   }else if (i%5==0){
-  return "buzz "
+  return "buzz"
   }else{
   return ` ${i}`;
   }
 }
 
 for (var i = 1; i <= limit; i++) {
-  final.push(fizzbuzz(i))
+  final.push(fizzbuzz(i)+", ");
 }
 document.querySelector("a").textContent = final
